89a424d264bdad7ddcbe748b585ce0c4af1beab4
Adam Lawrence <adam@impostertwo.com>
2024-02-04 19:53:58 -0600

    es

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,13 @@
+import profilePic from "./assets/profile.jpg"
+
+function Card(){
+    return(
+        <div className="card">
+            <img className="cardImage" alt="profile picture" src={profilePic}></img>
+            <h2 className="cardTitle">Adam</h2>
+            <p className="cardText">I am studying CS and networking. </p>
+        </div>
+    );
+}
+
+export default Card
