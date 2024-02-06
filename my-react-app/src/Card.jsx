1de2b68bf706347516d20a2a881641e49a44f627
Adam Lawrence <adam@impostertwo.com>
2024-02-05 20:18:22 -0600

    dsa

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -4,7 +4,7 @@ function Card(){
     return(
         <div className="card">
             <img className="cardImage" alt="profile picture" src={profilePic}></img>
-            <h2 className="cardTitle">Adam</h2>
+            <h2 className="cardTitle">Adam Two</h2>
             <p className="cardText">I am studying CS and networking. </p>
         </div>
     );
